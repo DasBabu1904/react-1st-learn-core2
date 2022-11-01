d2de9df1a2ae5d93e78776d18ee1afe9628d0401
Das_Babu <dass7550@gmail.com>
2022-11-01 15:36:10 +0600

    more state example using data fetch

--- src/App.js
+++ src/App.js
@@ -1,38 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternelUser></ExternelUser>
     </div>
   );
 }
 
 
 
-function Counter() {
-  const [count, setCount] = useState(0)
+function ExternelUser() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+  }, [])
 
 
-
-  const Incriment = () => setCount(count + 1);
-  const Decrement = () => setCount(count - 1);
-
-
-  // const Incriment = () => {
-  //   const newCount = count + 1;
-  //   setCount(newCount);
-  // }
   return (
     <div>
-      <h1>Count:{count}</h1>
-      <button onClick={Incriment}>Incriment</button>
-      <button onClick={Decrement}>Decrement</button>
+      <h1>External User</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
     </div>
   )
 }
+
+function User(props) {
+  return (
+    <div className="Details_Style">
+      <h3>Name:{props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Counter></Counter>
+//     </div>
+//   );
+// }
+
+
+
+// function Counter() {
+//   const [count, setCount] = useState(0)
+
+
+
+//   const Incriment = () => setCount(count + 1);
+//   const Decrement = () => setCount(count - 1);
+
+
+//   // const Incriment = () => {
+//   //   const newCount = count + 1;
+//   //   setCount(newCount);
+//   // }
+//   return (
+//     <div>
+//       <h1>Count:{count}</h1>
+//       <button onClick={Incriment}>Incriment</button>
+//       <button onClick={Decrement}>Decrement</button>
+//     </div>
+//   )
+// }
 
 // function Product(props) {
 //   return (
